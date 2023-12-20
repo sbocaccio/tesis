fec79967a83831d4021a8f9e2bcc0c1242ac42e7
Sebastian bocaccio <sbocaccio@dc.uba.ar>
2023-12-20 16:28:16 -0300

    Hello world con print externo

---
+++ hello_world/main/print.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+// Function to add two integers and print the result
+void printFromAnotherFile() {
+    printf("Printing from another file");
+}
---
+++ hello_world/main/print.h
@@ -0,0 +1,7 @@
+#ifndef ADD_H
+#define ADD_H
+
+// Function declaration
+void printFromAnotherFile();
+
+#endif
